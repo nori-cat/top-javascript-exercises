8de93ecb53be146b603c89871e9e0fd3426c2740
nori-cat <rachelngocxle@gmail.com>
2026-05-19 16:48:53 -0600

    08 done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    let resultArr = [];
+
+    let index = array.indexOf(element);
+
+    for (let i = 0; i < array.length; i++) {
+        if (i === index) {
+            continue;
+        } else {
+            resultArr.push(array[i]);
+        }
+    }
+
+    return resultArr;
 };
 
 // Do not edit below this line
