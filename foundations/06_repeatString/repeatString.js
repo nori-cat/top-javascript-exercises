636fda7c99fa436b521830a23c88df7d8b9233e0
nori-cat <rachelngocxle@gmail.com>
2026-05-19 16:23:12 -0600

    06 done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let repeatString = '';
 
+    if (num < 0) {
+        repeatString += 'ERROR';
+    } else {
+        for (i = 0; i < num; i++) {
+            repeatString += word;
+        }
+    }
+
+    return repeatString;
 };
 
 // Do not edit below this line
