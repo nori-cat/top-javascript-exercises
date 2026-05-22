4e33366ac61ab189ca8b3b5f65b99a9654dd3b11
nori-cat <rachelngocxle@gmail.com>
2026-05-22 16:49:23 -0600

    10 done

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    isDivisibleByFour = year % 4 == 0;
+    isDivisibleByFourHundred = year % 400 == 0;
+    isDivisibleByOneHundred = year % 100 == 0;
 
+    if (isDivisibleByFour && !isDivisibleByOneHundred || isDivisibleByFourHundred) {
+        return true;
+    }
+
+
+    return false;
 };
 
 // Do not edit below this line
