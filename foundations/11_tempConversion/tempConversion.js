f1878adc1d8e8aff692a1bdbd0ef5663723c6468
nori-cat <rachelngocxle@gmail.com>
2026-05-22 16:58:36 -0600

    11 done

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celc = (temp - 32) * (5/9);
+
+  if (celc == 0) {
+    return 0;
+  }
+
+  return celc.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahr = (temp * (9/5) + 32);
+
+  if (fahr == 0) {
+    return 0;
+  }
+  
+  return fahr.toFixed(1);
 };
 
 // Do not edit below this line
