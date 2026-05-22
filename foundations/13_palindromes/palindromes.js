d0b7f3ab67058d810d432086693d53f562138686
nori-cat <rachelngocxle@gmail.com>
2026-05-22 17:37:29 -0600

    13 done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    reverse = '';
+    for (i = word.length; i >= 0; i--) {
+        reverse += word.charAt(i);
+    }
 
+    if (reverse == word) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
