0c11e9fa939cd8ee88ac1d701980f4f5a98e4609
nori-cat <rachelngocxle@gmail.com>
2026-05-22 17:27:51 -0600

    12 done!

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num2 - num1;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  prod = 0;
+  for (i = 0; i < arr.length; i++) {
+    prod *= arr[i];
+  }
 
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	result = base;
+  for (i = 0; i < exp; i++) {
+    result *= base;
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	result = 0;
+  for (i = num; i > 0; i--) {
+    prod = i;
+    for (j = i - 1; j > 0; j--) {
+      prod *= j;
+    }
+
+    prod += result;
+
+  }
+
+  return result;
 };
 
 // Do not edit below this line
