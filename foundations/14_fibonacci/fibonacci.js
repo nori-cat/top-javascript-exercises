231205fc3fafb229ad8dd72a1154950b79abb986
nori-cat <rachelngocxle@gmail.com>
2026-05-24 17:26:30 -0600

    14 done

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 2) {
+        return num;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 };
 
 // Do not edit below this line
