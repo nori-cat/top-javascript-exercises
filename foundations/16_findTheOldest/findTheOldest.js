19208f6e5a1bdda1402d1a57e8d4971a9dde8223
nori-cat <rachelngocxle@gmail.com>
2026-05-24 17:52:05 -0600

    16 done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let ages = [];
+
+    
+    for (i = 0; i < people.length; i++) {
+        let age = people[i].yearOfDeath - people[i].yearOfBirth;
+        ages.push(age);
+    }
+
+    let oldest = 0;
+    
+    for (i = 0; i < ages.length; i++) {
+        if (ages[i] > oldest) {
+            oldest = ages[i];
+        }
+    }
+
+    let oldestIdx = ages.indexOf(oldest);
+
+    return people[oldestIdx];
 
 };
 
